08df262cf89ba6624c4345580a932ea419655ff3
Sasaya <storyn26383@gmail.com>
2023-09-26 13:59:15 +0800

    feat: use class map instead

--- src/dispatcher/class_map/HttpRequestHandler.php
+++ src/dispatcher/class_map/HttpRequestHandler.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace SwooleTW\Hyperf\Dispatcher;
+namespace Hyperf\Dispatcher;
 
 use Hyperf\Dispatcher\AbstractRequestHandler;
 use InvalidArgumentException;
--- src/dispatcher/src/ConfigProvider.php
+++ src/dispatcher/src/ConfigProvider.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 
 namespace SwooleTW\Hyperf\Dispatcher;
 
-use Hyperf\Dispatcher\HttpDispatcher as HyperfHttpDispatcher;
+use Hyperf\Dispatcher\HttpRequestHandler;
 
 class ConfigProvider
 {
     public function __invoke(): array
     {
         return [
-            'dependencies' => [
-                HyperfHttpDispatcher::class => HttpDispatcher::class,
+            'annotations' => [
+                'scan' => [
+                    'class_map' => [
+                        HttpRequestHandler::class => __DIR__ . '/../class_map/HttpRequestHandler.php',
+                    ],
+                ],
             ],
         ];
     }
