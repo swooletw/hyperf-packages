fbe574ef2d2b718e819cb20e18fc3c4ed2f999a4
Sasaya <storyn26383@gmail.com>
2023-09-26 10:59:40 +0800

    feat: add gate traits

---
+++ tests/Auth/Stub/AuthorizesRequestsStub.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SwooleTW\Hyperf\Tests\Auth\Stub;
+
+use SwooleTW\Hyperf\Auth\Access\AuthorizesRequests;
+
+class AuthorizesRequestsStub
+{
+    use AuthorizesRequests;
+}
