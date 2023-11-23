c25ea503001663c586aff93d0260a197fd5f45fe
Sasaya <storyn26383@gmail.com>
2023-11-23 16:57:07 +0800

    fix: a void function must not return a value

--- src/auth/src/Middleware/Authenticate.php
+++ src/auth/src/Middleware/Authenticate.php
@@ -44,7 +44,8 @@ class Authenticate implements MiddlewareInterface
 
         foreach ($guards as $guard) {
             if ($this->auth->guard($guard)->check()) {
-                return $this->auth->shouldUse($guard);
+                $this->auth->shouldUse($guard);
+                return;
             }
         }
 
