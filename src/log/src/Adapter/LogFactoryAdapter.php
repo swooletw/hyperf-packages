35e255dfbc4b856b6af00b715006f62ea298bbf5
Albert Chen <albert.cht@gmail.com>
2024-02-25 22:44:33 +0800

    chore: add compatibility comment in LogFactoryAdapter

--- src/log/src/Adapter/LogFactoryAdapter.php
+++ src/log/src/Adapter/LogFactoryAdapter.php
@@ -9,11 +9,13 @@ use Psr\Log\LoggerInterface;
 
 class LogFactoryAdapter extends LoggerFactory
 {
+    // name and group parameters are not applicable here
     public function make($name = 'hyperf', $group = 'default'): LoggerInterface
     {
         return $this->container->get(LoggerInterface::class)->channel();
     }
 
+    // name and group parameters are not applicable here
     public function get($name = 'hyperf', $group = 'default'): LoggerInterface
     {
         return $this->container->get(LoggerInterface::class)->channel();
