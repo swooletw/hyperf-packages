2189d75ee10f6d2c37cf038fd2044fe45f392ec5
Yu Shing <yushing@universetech.cc>
2024-03-18 15:19:35 +0800

    add cookie test

---
+++ tests/Cookie/Middleware/AddQueuedCookiesToResponseTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SwooleTW\Hyperf\Tests\Cookie;
+
+use Mockery as m;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use SwooleTW\Hyperf\Cookie\Contracts\Cookie as ContractsCookie;
+use SwooleTW\Hyperf\Cookie\Middleware\AddQueuedCookiesToResponse;
+use SwooleTW\Hyperf\Tests\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class AddQueuedCookiesToResponseTest extends TestCase
+{
+    public function testProcess()
+    {
+        $cookie = m::mock(ContractsCookie::class);
+        $cookie->shouldReceive('getQueuedCookies')->once()->andReturn(['cookie']);
+
+        $request = m::mock(ServerRequestInterface::class);
+        $response = m::mock(ResponseInterface::class);
+        $response->shouldReceive('withCookie')->once()->with('cookie')->andReturnSelf();
+
+        $handler = m::mock(RequestHandlerInterface::class);
+        $handler->shouldReceive('handle')->with($request)->once()->andReturn($response);
+
+
+        $middle = new AddQueuedCookiesToResponse($cookie);
+
+        $middle->process($request, $handler);
+    }
+}
