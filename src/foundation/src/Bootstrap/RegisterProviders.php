89da7d52825d87f25984b2ce43ee99d0aaf514a9
Albert Chen <albert.cht@gmail.com>
2024-07-14 18:24:08 +0800

    feat: support extra providers in composer.json

--- src/foundation/src/Bootstrap/RegisterProviders.php
+++ src/foundation/src/Bootstrap/RegisterProviders.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace SwooleTW\Hyperf\Foundation\Bootstrap;
 
+use Hyperf\Collection\Arr;
 use Hyperf\Contract\ConfigInterface;
+use Hyperf\Support\Composer;
 use SwooleTW\Hyperf\Foundation\Contracts\Application as ApplicationContract;
 
 class RegisterProviders
@@ -14,11 +16,20 @@ class RegisterProviders
      */
     public function bootstrap(ApplicationContract $app): void
     {
-        $providers = $app->get(ConfigInterface::class)
-            ->get('app.providers', []);
+        $providers = array_map(
+            fn (array $package) => Arr::wrap(($package['hyperf']['providers'] ?? []) ?? []),
+            Composer::getMergedExtra()
+        );
+        $providers = array_unique(
+            array_merge(
+                Arr::flatten($providers),
+                $app->get(ConfigInterface::class)->get('app.providers', [])
+            )
+        );
 
         foreach ($providers as $providerClass) {
             $app->register($providerClass);
         }
     }
+
 }
