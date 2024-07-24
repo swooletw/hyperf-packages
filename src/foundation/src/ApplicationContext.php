8731a6948047c4ecfda1cd88043326d786bf47ac
Albert Chen <albert.cht@gmail.com>
2024-07-25 00:10:46 +0800

    feat: support method call with auto bindings in command

--- src/foundation/src/ApplicationContext.php
+++ src/foundation/src/ApplicationContext.php
@@ -5,5 +5,15 @@ declare(strict_types=1);
 namespace SwooleTW\Hyperf\Foundation;
 
 use Hyperf\Context\ApplicationContext as HyperfApplicationContext;
+use SwooleTW\Hyperf\Container\Contracts\Container as ContainerContract;
 
-class ApplicationContext extends HyperfApplicationContext {}
+class ApplicationContext extends HyperfApplicationContext
+{
+    /**
+     * @throws TypeError
+     */
+    public static function getContainer(): ContainerContract
+    {
+        return self::$container;
+    }
+}
--- src/foundation/src/Console/Command.php
+++ src/foundation/src/Console/Command.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace SwooleTW\Hyperf\Foundation\Console;
 
+use function Hyperf\Coroutine\run;
 use Hyperf\Command\Command as HyperfCommand;
+use Hyperf\Command\Event\AfterExecute;
+use Hyperf\Command\Event\AfterHandle;
+use Hyperf\Command\Event\BeforeHandle;
+use Hyperf\Command\Event\FailToHandle;
+use Hyperf\Coroutine\Coroutine;
+use Swoole\ExitException;
+use SwooleTW\Hyperf\Foundation\ApplicationContext;
+use Symfony\Component\Console\Input\InputInterface;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
 
 abstract class Command extends HyperfCommand
 {
@@ -78,5 +90,49 @@ abstract class Command extends HyperfCommand
     public function options()
     {
         return $this->option();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->disableDispatcher($input);
+        $method = method_exists($this, 'handle') ? 'handle' : '__invoke';
+
+        $callback = function () use ($method): int {
+            try {
+                $this->eventDispatcher?->dispatch(new BeforeHandle($this));
+                $statusCode = ApplicationContext::getContainer()
+                    ->call([$this, $method]);
+                if (is_int($statusCode)) {
+                    $this->exitCode = $statusCode;
+                }
+                $this->eventDispatcher?->dispatch(new AfterHandle($this));
+            } catch (Throwable $exception) {
+                if (class_exists(ExitException::class) && $exception instanceof ExitException) {
+                    return $this->exitCode = (int) $exception->getStatus();
+                }
+
+                if (!$this->eventDispatcher) {
+                    throw $exception;
+                }
+
+                $this->getApplication()?->renderThrowable($exception, $this->output);
+
+                $this->exitCode = self::FAILURE;
+
+                $this->eventDispatcher->dispatch(new FailToHandle($this, $exception));
+            } finally {
+                $this->eventDispatcher?->dispatch(new AfterExecute($this, $exception ?? null));
+            }
+
+            return $this->exitCode;
+        };
+
+        if ($this->coroutine && ! Coroutine::inCoroutine()) {
+            run($callback, $this->hookFlags);
+        } else {
+            $callback();
+        }
+
+        return $this->exitCode >= 0 && $this->exitCode <= 255 ? $this->exitCode : self::INVALID;
     }
 }
