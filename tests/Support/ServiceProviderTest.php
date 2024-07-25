7dff8c403b7f3167c1c084927cdc167a7d2d7187
Albert Chen <albert.cht@gmail.com>
2024-07-25 20:55:58 +0800

    feat: add ServiceProviderTest

---
+++ tests/Support/ServiceProviderTest.php
@@ -0,0 +1,172 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SwooleTW\Hyperf\Tests\Support;
+
+use Mockery as m;
+use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\TestCase;
+use SwooleTW\Hyperf\Foundation\Application;
+use SwooleTW\Hyperf\Support\ServiceProvider;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class SupportServiceProviderTest extends TestCase
+{
+    protected Application $app;
+
+    protected function setUp(): void
+    {
+        ServiceProvider::$publishes = [];
+        ServiceProvider::$publishGroups = [];
+
+        $this->app = $app = m::mock(Application::class)->makePartial();
+
+        $one = new ServiceProviderForTestingOne($app);
+        $one->boot();
+        $two = new ServiceProviderForTestingTwo($app);
+        $two->boot();
+    }
+
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+
+    public function testPublishableServiceProviders()
+    {
+        $toPublish = ServiceProvider::publishableProviders();
+        $expected = [
+            ServiceProviderForTestingOne::class,
+            ServiceProviderForTestingTwo::class,
+        ];
+        $this->assertEquals($expected, $toPublish, 'Publishable service providers do not return expected set of providers.');
+    }
+
+    public function testPublishableGroups()
+    {
+        $toPublish = ServiceProvider::publishableGroups();
+        $this->assertEquals([
+            'some_tag',
+            'tag_one',
+            'tag_two',
+            'tag_three',
+            'tag_four',
+            'tag_five',
+        ], $toPublish, 'Publishable groups do not return expected set of groups.');
+    }
+
+    public function testSimpleAssetsArePublishedCorrectly()
+    {
+        $toPublish = ServiceProvider::pathsToPublish(ServiceProviderForTestingOne::class);
+        $this->assertArrayHasKey('source/unmarked/one', $toPublish, 'Service provider does not return expected published path key.');
+        $this->assertArrayHasKey('source/tagged/one', $toPublish, 'Service provider does not return expected published path key.');
+        $this->assertEquals([
+            'source/unmarked/one' => 'destination/unmarked/one',
+            'source/tagged/one' => 'destination/tagged/one',
+            'source/tagged/multiple' => 'destination/tagged/multiple',
+            'source/unmarked/two' => 'destination/unmarked/two',
+            'source/tagged/three' => 'destination/tagged/three',
+            'source/tagged/multiple_two' => 'destination/tagged/multiple_two',
+        ], $toPublish, 'Service provider does not return expected set of published paths.');
+    }
+
+    public function testMultipleAssetsArePublishedCorrectly()
+    {
+        $toPublish = ServiceProvider::pathsToPublish(ServiceProviderForTestingTwo::class);
+        $this->assertArrayHasKey('source/unmarked/two/a', $toPublish, 'Service provider does not return expected published path key.');
+        $this->assertArrayHasKey('source/unmarked/two/b', $toPublish, 'Service provider does not return expected published path key.');
+        $this->assertArrayHasKey('source/unmarked/two/c', $toPublish, 'Service provider does not return expected published path key.');
+        $this->assertArrayHasKey('source/tagged/two/a', $toPublish, 'Service provider does not return expected published path key.');
+        $this->assertArrayHasKey('source/tagged/two/b', $toPublish, 'Service provider does not return expected published path key.');
+        $expected = [
+            'source/unmarked/two/a' => 'destination/unmarked/two/a',
+            'source/unmarked/two/b' => 'destination/unmarked/two/b',
+            'source/unmarked/two/c' => 'destination/tagged/two/a',
+            'source/tagged/two/a' => 'destination/tagged/two/a',
+            'source/tagged/two/b' => 'destination/tagged/two/b',
+        ];
+        $this->assertEquals($expected, $toPublish, 'Service provider does not return expected set of published paths.');
+    }
+
+    public function testSimpleTaggedAssetsArePublishedCorrectly()
+    {
+        $toPublish = ServiceProvider::pathsToPublish(ServiceProviderForTestingOne::class, 'some_tag');
+        $this->assertArrayNotHasKey('source/tagged/two/a', $toPublish, 'Service provider does return unexpected tagged path key.');
+        $this->assertArrayNotHasKey('source/tagged/two/b', $toPublish, 'Service provider does return unexpected tagged path key.');
+        $this->assertArrayHasKey('source/tagged/one', $toPublish, 'Service provider does not return expected tagged path key.');
+        $this->assertEquals(['source/tagged/one' => 'destination/tagged/one'], $toPublish, 'Service provider does not return expected set of published tagged paths.');
+    }
+
+    public function testMultipleTaggedAssetsArePublishedCorrectly()
+    {
+        $toPublish = ServiceProvider::pathsToPublish(ServiceProviderForTestingTwo::class, 'some_tag');
+        $this->assertArrayHasKey('source/tagged/two/a', $toPublish, 'Service provider does not return expected tagged path key.');
+        $this->assertArrayHasKey('source/tagged/two/b', $toPublish, 'Service provider does not return expected tagged path key.');
+        $this->assertArrayNotHasKey('source/tagged/one', $toPublish, 'Service provider does return unexpected tagged path key.');
+        $this->assertArrayNotHasKey('source/unmarked/two/c', $toPublish, 'Service provider does return unexpected tagged path key.');
+        $expected = [
+            'source/tagged/two/a' => 'destination/tagged/two/a',
+            'source/tagged/two/b' => 'destination/tagged/two/b',
+        ];
+        $this->assertEquals($expected, $toPublish, 'Service provider does not return expected set of published tagged paths.');
+    }
+
+    public function testMultipleTaggedAssetsAreMergedCorrectly()
+    {
+        $toPublish = ServiceProvider::pathsToPublish(null, 'some_tag');
+        $this->assertArrayHasKey('source/tagged/two/a', $toPublish, 'Service provider does not return expected tagged path key.');
+        $this->assertArrayHasKey('source/tagged/two/b', $toPublish, 'Service provider does not return expected tagged path key.');
+        $this->assertArrayHasKey('source/tagged/one', $toPublish, 'Service provider does not return expected tagged path key.');
+        $this->assertArrayNotHasKey('source/unmarked/two/c', $toPublish, 'Service provider does return unexpected tagged path key.');
+        $expected = [
+            'source/tagged/one' => 'destination/tagged/one',
+            'source/tagged/two/a' => 'destination/tagged/two/a',
+            'source/tagged/two/b' => 'destination/tagged/two/b',
+        ];
+        $this->assertEquals($expected, $toPublish, 'Service provider does not return expected set of published tagged paths.');
+    }
+
+    public function testPublishesMigrations()
+    {
+        $serviceProvider = new ServiceProviderForTestingOne($this->app);
+
+        (fn () => $this->publishesMigrations(['source/tagged/four' => 'destination/tagged/four'], 'tag_four'))
+            ->call($serviceProvider);
+
+        $this->assertContains('source/tagged/four', ServiceProvider::publishableMigrationPaths());
+    }
+}
+
+class ServiceProviderForTestingOne extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot()
+    {
+        $this->publishes(['source/unmarked/one' => 'destination/unmarked/one']);
+        $this->publishes(['source/tagged/one' => 'destination/tagged/one'], 'some_tag');
+        $this->publishes(['source/tagged/multiple' => 'destination/tagged/multiple'], ['tag_one', 'tag_two']);
+
+        $this->publishesMigrations(['source/unmarked/two' => 'destination/unmarked/two']);
+        $this->publishesMigrations(['source/tagged/three' => 'destination/tagged/three'], 'tag_three');
+        $this->publishesMigrations(['source/tagged/multiple_two' => 'destination/tagged/multiple_two'], ['tag_four', 'tag_five']);
+    }
+}
+
+class ServiceProviderForTestingTwo extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot()
+    {
+        $this->publishes(['source/unmarked/two/a' => 'destination/unmarked/two/a']);
+        $this->publishes(['source/unmarked/two/b' => 'destination/unmarked/two/b']);
+        $this->publishes(['source/unmarked/two/c' => 'destination/tagged/two/a']);
+        $this->publishes(['source/tagged/two/a' => 'destination/tagged/two/a'], 'some_tag');
+        $this->publishes(['source/tagged/two/b' => 'destination/tagged/two/b'], 'some_tag');
+    }
+}
