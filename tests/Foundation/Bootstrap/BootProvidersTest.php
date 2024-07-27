80c5a57586b7d5a4624844903d080903300f06f4
Albert Chen <albert.cht@gmail.com>
2024-07-27 19:46:10 +0800

    feat: add BootProvidersTest

---
+++ tests/Foundation/Bootstrap/BootProvidersTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SwooleTW\Hyperf\Tests\Foundation\Bootstrap;
+
+use SwooleTW\Hyperf\Foundation\Bootstrap\BootProviders;
+use SwooleTW\Hyperf\Support\ServiceProvider;
+use SwooleTW\Hyperf\Tests\Foundation\Concerns\HasMockedApplication;
+use SwooleTW\Hyperf\Tests\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class BootProvidersTest extends TestCase
+{
+    use HasMockedApplication;
+
+    public function testBoot()
+    {
+        $app = $this->getApplication();
+        $app->register(ApplicationBasicServiceProviderStub::class);
+
+        (new BootProviders)->bootstrap($app);
+
+        $this->assertSame('bar', $app->get('foo'));
+    }
+
+}
+
+class ApplicationBasicServiceProviderStub extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->app->bind('foo', fn () => 'bar');
+    }
+}
