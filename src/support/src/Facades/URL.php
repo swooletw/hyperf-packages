724d3925b8ed91b4ab88ec3b67c113a8549f20a0
Albert Chen <albert.cht@gmail.com>
2024-09-01 22:47:24 +0800

    feat: add phpdoc to url facade

--- src/support/src/Facades/URL.php
+++ src/support/src/Facades/URL.php
@@ -10,6 +10,8 @@ use SwooleTW\Hyperf\Router\UrlGenerator;
  * @method static string route(string $name, array $parameters = [], string $server = 'http')
  * @method static string to(string $path, array $extra = [], ?bool $secure = null)
  * @method static string secure(string $path, array $extra = [])
+ * @method static string full()
+ * @method static string current()
  *
  * @see UrlGenerator
  */
