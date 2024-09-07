8449930ca92d84e210a1177fa49de94e633680e4
Albert Chen <albert.cht@gmail.com>
2024-09-07 14:36:24 +0800

    feat: add redirectTo in AuthenticationException

--- src/auth/src/AuthenticationException.php
+++ src/auth/src/AuthenticationException.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SwooleTW\Hyperf\Auth;
 
 use Exception;
+use Psr\Http\Message\RequestInterface;
 
 class AuthenticationException extends Exception
 {
@@ -14,15 +15,28 @@ class AuthenticationException extends Exception
     protected array $guards;
 
     /**
+     * The path the user should be redirected to.
+     */
+    protected ?string $redirectTo;
+
+    /**
+     * The callback that should be used to generate the authentication redirect path.
+     *
+     * @var callable
+     */
+    protected static $redirectToCallback;
+
+    /**
      * Create a new authentication exception.
      *
      * @param string $message
      */
-    public function __construct($message = 'Unauthenticated.', array $guards = [])
+    public function __construct($message = 'Unauthenticated.', array $guards = [], ?string $redirectTo = null)
     {
         parent::__construct($message);
 
         $this->guards = $guards;
+        $this->redirectTo = $redirectTo;
     }
 
     /**
@@ -33,5 +47,27 @@ class AuthenticationException extends Exception
     public function guards()
     {
         return $this->guards;
+    }
+
+    /**
+     * Get the path the user should be redirected to.
+     */
+    public function redirectTo(RequestInterface $request): ?string
+    {
+        if ($this->redirectTo) {
+            return $this->redirectTo;
+        }
+
+        if (static::$redirectToCallback) {
+            return call_user_func(static::$redirectToCallback, $request);
+        }
+    }
+
+    /**
+     * Specify the callback that should be used to generate the redirect path.
+     */
+    public static function redirectUsing(callable $redirectToCallback): void
+    {
+        static::$redirectToCallback = $redirectToCallback;
     }
 }
