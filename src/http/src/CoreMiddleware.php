4a3c961f85a7959321309b896ddcc16f02abbb8b
Albert Chen <albert.cht@gmail.com>
2024-11-12 16:42:39 +0800

    feat: suppport callAction while dispathcing controller action

--- src/http/src/CoreMiddleware.php
+++ src/http/src/CoreMiddleware.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace SwooleTW\Hyperf\Http;
 
+use Closure;
 use Hyperf\Contract\Arrayable;
 use Hyperf\Contract\Jsonable;
 use Hyperf\HttpMessage\Stream\SwooleStream;
 use Hyperf\HttpServer\CoreMiddleware as HyperfCoreMiddleware;
+use Hyperf\HttpServer\Router\Dispatched;
 use Hyperf\View\RenderInterface;
 use Hyperf\ViewEngine\Contract\ViewInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use SwooleTW\Hyperf\HttpMessage\Exceptions\ServerErrorHttpException;
 use Swow\Psr7\Message\ResponsePlusInterface;
 
 class CoreMiddleware extends HyperfCoreMiddleware
@@ -30,5 +33,33 @@ class CoreMiddleware extends HyperfCoreMiddleware
         }
 
         return parent::transferToResponse($response, $request);
+    }
+
+    /**
+     * Handle the response when found.
+     *
+     * @return array|Arrayable|mixed|ResponseInterface|string
+     */
+    protected function handleFound(Dispatched $dispatched, ServerRequestInterface $request): mixed
+    {
+        if ($dispatched->handler->callback instanceof Closure) {
+            $parameters = $this->parseClosureParameters($dispatched->handler->callback, $dispatched->params);
+            $callback = $dispatched->handler->callback;
+
+            return $callback(...$parameters);
+        }
+
+        [$controller, $action] = $this->prepareHandler($dispatched->handler->callback);
+        $controllerInstance = $this->container->get($controller);
+        if (! method_exists($controllerInstance, $action)) {
+            throw new ServerErrorHttpException("{$controller}@{$action} does not exist.");
+        }
+
+        $parameters = $this->parseMethodParameters($controller, $action, $dispatched->params);
+        if (method_exists($controllerInstance, 'callAction')) {
+            return $controllerInstance->callAction($action, $parameters);
+        }
+
+        return $controllerInstance->{$action}(...$parameters);
     }
 }
