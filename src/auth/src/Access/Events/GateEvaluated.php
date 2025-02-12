80c366cff8b573db4af2a4016a7521769250982c
Albert Chen <albert.cht@gmail.com>
2025-02-12 11:08:17 +0800

    fix: fix type hint for result in GateEvaluated event

--- src/auth/src/Access/Events/GateEvaluated.php
+++ src/auth/src/Access/Events/GateEvaluated.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SwooleTW\Hyperf\Auth\Access\Events;
 
+use SwooleTW\Hyperf\Auth\Access\Response;
 use SwooleTW\Hyperf\Auth\Contracts\Authenticatable;
 
 class GateEvaluated
@@ -13,13 +14,13 @@ class GateEvaluated
      *
      * @param null|Authenticatable $user the authenticatable model
      * @param string $ability the ability being evaluated
-     * @param null|bool $result the result of the evaluation
+     * @param null|bool|Response $result the result of the evaluation
      * @param array $arguments the arguments given during evaluation
      */
     public function __construct(
         public ?Authenticatable $user,
         public string $ability,
-        public ?bool $result,
+        public null|bool|Response $result,
         public array $arguments
     ) {
     }
