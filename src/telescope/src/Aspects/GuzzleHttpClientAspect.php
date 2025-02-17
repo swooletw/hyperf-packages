dc96cbd6bd60ffd94e1b2ffd1676249957777f5f
Albert Chen <albert.cht@gmail.com>
2025-02-17 14:14:40 +0800

    improve: improve GuzzleHttpClientAspect in telescope

--- src/telescope/src/Aspects/GuzzleHttpClientAspect.php
+++ src/telescope/src/Aspects/GuzzleHttpClientAspect.php
@@ -11,14 +11,12 @@ use Hyperf\Contract\ConfigInterface;
 use Hyperf\Di\Aop\AbstractAspect;
 use Hyperf\Di\Aop\ProceedingJoinPoint;
 use Hyperf\Stringable\Str;
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use SwooleTW\Hyperf\Telescope\IncomingEntry;
 use SwooleTW\Hyperf\Telescope\Telescope;
 use Throwable;
 
-/**
- * This file is originated from friendsofhyperf/telescope.
- */
 class GuzzleHttpClientAspect extends AbstractAspect
 {
     public array $classes = [
@@ -56,23 +54,23 @@ class GuzzleHttpClientAspect extends AbstractAspect
         $onStats = $options['on_stats'] ?? null;
         $proceedingJoinPoint->arguments['keys']['options']['on_stats'] = function (TransferStats $stats) use ($onStats) {
             try {
-                $request = $stats->getRequest();
-                $response = $stats->getResponse();
-                $content = [
-                    'method' => $request->getMethod(),
-                    'uri' => (string) $request->getUri(),
-                    'headers' => $request->getHeaders(),
-                    'duration' => $stats->getTransferTime() * 1000,
-                ];
+                $content = $this->getRequest(
+                    $request = $stats->getRequest(),
+                    $stats
+                );
 
                 if ($response = $stats->getResponse()) {
-                    $content['response_status'] = $response->getStatusCode();
-                    $content['response_headers'] = $response->getHeaders();
-                    $content['response'] = $this->getResponse($response);
+                    $content = array_merge(
+                        $content,
+                        $this->getResponse($response)
+                    );
                 }
 
-                Telescope::recordClientRequest(IncomingEntry::make($content));
-            } catch (Throwable $exception) {
+                Telescope::recordClientRequest(
+                    IncomingEntry::make($content)
+                        ->tags([$request->getUri()->getHost()])
+                );
+            } catch (Throwable $e) {
                 // We will catch the exception to prevent the request from being interrupted.
             }
 
@@ -84,7 +82,62 @@ class GuzzleHttpClientAspect extends AbstractAspect
         return $proceedingJoinPoint->process();
     }
 
-    public function getResponse(ResponseInterface $response): ?string
+    protected function getRequest(RequestInterface $request, TransferStats $stats): array
+    {
+        return [
+            'method' => $request->getMethod(),
+            'uri' => (string) $request->getUri(),
+            'headers' => $this->headers($request->getHeaders()),
+            'payload' => $this->getRequestPayload($request),
+            'duration' => floor($stats->getTransferTime() * 1000),
+        ];
+    }
+
+    /**
+     * Extract the payload from the given request.
+     */
+    protected function getRequestPayload(RequestInterface $request): array|string
+    {
+        $stream = $request->getBody();
+        try {
+            if ($stream->isSeekable()) {
+                $stream->rewind();
+            }
+
+            $sizeLimit = ($this->options['request_size_limit'] ?? 64) * 1024;
+            if ($stream->getSize() >= $sizeLimit) {
+                return $stream->read($sizeLimit) . ' (truncated...)';
+            }
+
+            $content = $stream->getContents();
+            if (is_array($decoded = json_decode($content, true))
+                && json_last_error() === JSON_ERROR_NONE
+            ) {
+                return $this->hideParameters($decoded, Telescope::$hiddenResponseParameters);
+            }
+
+            return $content;
+        } catch (Throwable $e) {
+            return 'Purged By Telescope: ' . $e->getMessage();
+        } finally {
+            if ($stream->isSeekable()) {
+                $stream->rewind();
+            }
+        }
+
+        return 'Unknown';
+    }
+
+    protected function getResponse(ResponseInterface $response): array
+    {
+        return [
+            'response_status' => $response->getStatusCode(),
+            'response_headers' => $response->getHeaders(),
+            'response' => $this->getResponsePayload($response),
+        ];
+    }
+
+    protected function getResponsePayload(ResponseInterface $response): array|string
     {
         $stream = $response->getBody();
         try {
@@ -92,22 +145,24 @@ class GuzzleHttpClientAspect extends AbstractAspect
                 $stream->rewind();
             }
 
-            $content = $stream->getContents();
+            $sizeLimit = ($this->options['response_size_limit'] ?? 64) * 1024;
+            if ($stream->getSize() >= $sizeLimit) {
+                return $stream->read($sizeLimit) . ' (truncated...)';
+            }
 
-            if (is_string($content)) {
-                if (! $this->contentWithinLimits($content)) {
-                    return 'Purged By Telescope';
-                }
-                if (is_array(json_decode($content, true))
-                    && json_last_error() === JSON_ERROR_NONE
-                ) {
-                    return $this->contentWithinLimits($content) /* @phpstan-ignore-line */
-                        ? $this->hideParameters(json_decode($content, true), Telescope::$hiddenResponseParameters)
-                        : 'Purged By Telescope';
-                }
-                if (Str::startsWith(strtolower($response->getHeaderLine('content-type') ?: ''), 'text/plain')) {
-                    return $this->contentWithinLimits($content) ? $content : 'Purged By Telescope'; /* @phpstan-ignore-line */
-                }
+            $content = $stream->getContents();
+            if (is_array($decoded = json_decode($content, true))
+                && json_last_error() === JSON_ERROR_NONE
+            ) {
+                return $this->hideParameters($decoded, Telescope::$hiddenResponseParameters);
+            }
+            if (Str::startsWith(strtolower($response->getHeaderLine('content-type') ?: ''), 'text/plain')) {
+                return $content;
+            }
+
+            $statusCode = $response->getStatusCode();
+            if ($statusCode >= 300 && $statusCode < 400) {
+                return 'Redirected to ' . $response->getHeaderLine('Location');
             }
 
             if (empty($content)) {
@@ -124,11 +179,25 @@ class GuzzleHttpClientAspect extends AbstractAspect
         return 'HTML Response';
     }
 
-    protected function contentWithinLimits(string $content): bool
+    /**
+     * Format the given headers.
+     */
+    protected function headers(array $headers): array
     {
-        $limit = $this->options['size_limit'] ?? 64;
+        $headerNames = array_map(function (string $headerName) {
+            return strtolower($headerName);
+        }, array_keys($headers));
 
-        return intdiv(mb_strlen($content), 1000) <= $limit;
+        $headerValues = array_map(function (array $header) {
+            return implode(', ', $header);
+        }, $headers);
+
+        $headers = array_combine($headerNames, $headerValues);
+
+        return $this->hideParameters(
+            $headers,
+            Telescope::$hiddenRequestHeaders
+        );
     }
 
     /**
